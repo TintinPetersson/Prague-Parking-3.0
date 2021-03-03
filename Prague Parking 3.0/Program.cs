73e3225c7480a540d75a2ea2b89f3d762b60c658
Jonathan Kullman <jontekulljobb@gmail.com>
2021-03-03 13:40:49 +0100

    UPDATE
    YEY

--- Prague Parking 3.0/Program.cs
+++ Prague Parking 3.0/Program.cs
@@ -3,6 +3,8 @@ namespace PragueParking_3._0
 {
     class Program
     {
+
+          
         static void Main(string[] args)
         {
             string conn = @"Data Source=LAPTOP-2UTSSLFS\SQLEXPRESS;Initial Catalog=PragueParking3;Integrated Security=True";
